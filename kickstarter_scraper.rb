56403f44240a76f96501893dd8ee3d0d46c9bbf4
Marcel Rico <personal.mr4@gmail.com>
2020-01-12 22:13:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,12 +12,13 @@ def create_project_hash
   
   project = kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    image = project.css("div.project-thumbnail a img").attribute("src").value
+    description = project.css("p.bbcard_blurb").text
+    projects[title.to_sym] = {
+      :image_link => }
   end
   
-  # 
-  # image: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_blurb").text
+  
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
   
   
